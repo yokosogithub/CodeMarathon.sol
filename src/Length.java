c0492e7e297e1ff18ad718346f83373df860de8b
yokosogithub <xiepeng1992@gmail.com>
2013-08-03 19:03:52 +0800

    Create Length.java

---
+++ src/Length.java
@@ -0,0 +1,12 @@
+package length;
+
+public class Length {
+   double num;
+   String name;
+   String names;
+   public Length(String name,double num)
+   {
+     this.name=name;
+	   this.num=num;
+   }
+}
